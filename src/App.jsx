b1f73e39303f21229a690d8d8c8d7bfa6f025d40
unknown <you@example.com>
2025-04-11 18:49:48 +0530

    Page Render Logic Pending

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import React from 'react';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import React,{useState} from 'react';
+import { LaptopOutlined, NotificationOutlined, UserOutlined, ProductOutlined } from '@ant-design/icons';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
+
 const { Header, Content, Footer, Sider } = Layout;
-const items1 = ['1', '2', '3'].map(key => ({
+
+const isLogin = true; //Hard quoated for now will change later
+const isAdmin = true; // Hard quoated for now will change later
+
+// const isLogin = false; //Hard quoated for now will change later
+// const isAdmin = false; // Hard quoated for now will change later
+
+const navItems = isLogin ? ["Home"] : ["Home", "Login"] ;
+
+const items1 = navItems.map(key => ({
   key,
-  label: `nav ${key}`,
+  label: `${key}`,
 }));
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+
+
+const subnav = isAdmin ? ["Manage Users", "Manage Products"] : ["Manage Products"];
+
+const subnavIcons = isAdmin ? [UserOutlined, ProductOutlined] : [ProductOutlined];
+
+const options = isAdmin ? [`All Users`,`Add User`, `All Products`, `Add Product`]: [`All Products`, `Add Product`];
+
+
+const sideBarChildrenLength = 2;
+
+const items2 = subnavIcons.map((icon, index) => {
   const key = String(index + 1);
   return {
     key: `sub${key}`,
     icon: React.createElement(icon),
-    label: `subnav ${key}`,
-    children: Array.from({ length: 4 }).map((_, j) => {
-      const subKey = index * 4 + j + 1;
+    label: subnav[index],
+    children: Array.from({ length: sideBarChildrenLength }).map((_, j) => {
+      const subKey = index * sideBarChildrenLength + j + 1;
       return {
         key: subKey,
-        label: `option${subKey}`,
+        label: options[subKey-1],
       };
     }),
   };
 });
 
-// const item3 = ["Home", "Company Name", "Admin/User"].map((page,)=>)
-
 const App = () => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
   return (
-    <Layout>
+    <Layout style={{height: "100vh"}}>
       <Header style={{ display: 'flex', alignItems: 'center' }}>
         <div className="demo-logo" />
         <Menu
@@ -43,11 +63,11 @@ const App = () => {
       <div style={{ padding: '0 48px' }}>
         <Breadcrumb style={{ margin: '16px 0' }}>
           <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
+          <Breadcrumb.Item>Company Name</Breadcrumb.Item>
+          <Breadcrumb.Item>Admin or User</Breadcrumb.Item>
         </Breadcrumb>
         <Layout
-          style={{ padding: '15px 0', background: colorBgContainer, borderRadius: borderRadiusLG }}
+          style={{ padding: '24px 0', background: colorBgContainer, borderRadius: borderRadiusLG }}
         >
           <Sider style={{ background: colorBgContainer }} width={200}>
             <Menu
@@ -61,8 +81,8 @@ const App = () => {
           <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
         </Layout>
       </div>
-      <Footer style={{ textAlign: 'center' }}>
-        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      <Footer style={{ textAlign: 'center', height: "10vh" }}>
+        Ecom Practice ©{new Date().getFullYear()} Created by Bhushan
       </Footer>
     </Layout>
   );
